39b62eea1a63fe68f3c37aa5313bc73eaedbca04
santoshx.dev <santoshx.dev@gmail.com>
2025-10-09 06:11:00 +0000

    update parent login password

--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -190,7 +190,7 @@ export async function addStudent(studentData: Student): Promise<void> {
                 name: studentData.parentName,
                 phone: studentData.parentMobile,
                 children: [admissionNumber],
-                password: `${studentData.parentName.split(' ')[0]}@${birthYear}`,
+                password: `${studentData.parentName.split(' ')[0]}@${studentData.parentMobile.slice(0, 4)}`,
             };
             batch.set(parentDocRef, newParent);
         }
